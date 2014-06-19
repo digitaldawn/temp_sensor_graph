7e982b4ad2bd98812d5da7c037ad21cd9858e253
Paul Millar <paul@digitaldawn.com.au>
2014-06-19 10:31:50 +1000

    Removed midnight reference mistake

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,7 +1,7 @@
 class ReadingsController < ApplicationController
   def index
     x = -1
-    @readings = Reading.where(created_at: (Time.now.midnight - 1.day)..Time.now.midnight).map { |reading| { x: x+=1, y: reading.temperature } }
+    @readings = Reading.where(created_at: (Time.now - 1.day)..Time.now).map { |reading| { x: x+=1, y: reading.temperature } }
     render json: @readings.to_json
   end
 
