9753748ef0af20af6d15c45e5da8af711734a8e3
Mark <mpwirwin@gmail.com>
2015-08-19 11:02:16 +1000

    converted x axis to timefloat

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,7 +1,7 @@
 class ReadingsController < ApplicationController
   def index
     #x = -1
-    @readings = Reading.where(created_at: (Time.now - 1.day)..Time.now).map { |reading| { x: reading.created_at , y: reading.temperature } }
+    @readings = Reading.where(created_at: (Time.now - 1.day)..Time.now).map { |reading| { x: reading.created_at.Time.to_f , y: reading.temperature } }
     render json: @readings.to_json
   end
 
