fe3b2c9791a8873f9d7d2e0f655e986ae9f29cb3
Mark <mpwirwin@gmail.com>
2015-08-19 15:05:37 +1000

    changed t to reading.created_at

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -3,7 +3,7 @@ class ReadingsController < ApplicationController
     #x = -1
     t = Time.now.to_i
 
-    @readings = Reading.where(created_at: (Time.now - 1.day)..Time.now).map { |reading| { x: (reading.created_at.strftime(“%M”).to_i - Time.now.strftime(“%M”).to_i) , y: reading.temperature } }
+    @readings = Reading.where(created_at: (Time.now - 1.day)..Time.now).map { |reading| { x: (reading.created_at.strftime(“%s”).to_i - Time.now.strftime(“%s”).to_i) , y: reading.temperature } }
     render json: @readings.to_json
   end
 
