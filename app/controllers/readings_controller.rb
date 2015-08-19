72193564925317d6827f91558455bb27311a7171
Mark <mpwirwin@gmail.com>
2015-08-19 11:29:00 +1000

    Time changes to integer

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,7 +1,9 @@
 class ReadingsController < ApplicationController
   def index
     #x = -1
-    @readings = Reading.where(created_at: (Time.now - 1.day)..Time.now).map { |reading| { x: reading.created_at.Time.to_f , y: reading.temperature } }
+    t = Time.now.to_i
+
+    @readings = Reading.where(created_at: (Time.now - 1.day)..Time.now).map { |reading| { x: reading.created_at.Time.to_i-t , y: reading.temperature } }
     render json: @readings.to_json
   end
 
