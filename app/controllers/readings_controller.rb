8270f13e1fd70d8bff755c82903d7c86ababf6ac
Mark <mpwirwin@gmail.com>
2016-01-14 12:23:07 +1100

    fix to make it work again

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,6 +1,6 @@
 class ReadingsController < ApplicationController
   def index
-    #x = -1
+    x = -1
     t = Time.now.to_i
     @readings = Reading.where(created_at: (Time.now - 1.year)..Time.now).map { |reading| { x: x+=1, y: reading.temperature } }
     #@readings = Reading.where(created_at: (Time.now - 1.day)..Time.now).map { |reading| { x: (reading.created_at.strftime(“%s”).to_i - Time.now.strftime(“%s”).to_i) , y: reading.temperature } }
