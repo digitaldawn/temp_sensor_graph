f2079705fac452a05647caa648b0af10c3768583
Mark Irwin <mpwirwin@gmail.com>
2016-01-19 23:32:08 +1100

    D3 wasn't working to changed to ChartJS, I have managed to graph some of the data, not figured out getting the JSON to work or setting up the xaxis to use datetime

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -2,8 +2,9 @@ class ReadingsController < ApplicationController
   def index
     x = -1
     t = Time.now.to_i
-    #@readings = Reading.where(created_at: (Time.now - 1.year)..Time.now).map { |reading| { x: (reading.created_at.strftime(“%Y-%m-%d-%H:%M”), y: reading.temperature } }
-    @readings = Reading.where(created_at: (Time.now - 1.day)..Time.now).map { |reading| { x: x+1 , y: reading.temperature } }
+    @readings = Reading.where(created_at: (Time.now - 1.year)..Time.now).map { |reading| { x: (reading.created_at.to_i), y: reading.temperature } }
+    
+    #@readings = Reading.where(created_at: (Time.now - 1.day)..Time.now).map { |reading| { x: x+1 , y: reading.temperature } }
     render json: @readings.to_json
   end
 
